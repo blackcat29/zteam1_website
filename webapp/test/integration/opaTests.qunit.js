a591344419baab201729ff4155f9bca844cb891c
Tien Nguyen <16.vantien@gmail.com>
2024-07-29 07:07:33 +0000

    Initial Commit @7/29/2024, 7:07:33 AM

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zteam1website/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zteam1website/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
